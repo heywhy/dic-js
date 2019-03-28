08bb7cea50a1dd4b3bd6aa601cc9e695117a3d49
Atanda Rasheed Ayomide <ratanda@byteworks.com.ng>
2019-03-28 13:40:05 +0100

    Updated docs

--- out/index.d.ts
+++ out/index.d.ts
@@ -3,6 +3,6 @@ import { ContainerInterface, ServiceFactory } from "./contracts";
 import Registrar from "./Registrar";
 export { Container };
 export declare const getContext: (id?: string | undefined) => ContainerInterface;
-export declare const bind: <T>(service: string | T | ServiceFactory, container?: ContainerInterface) => Registrar<T>;
+export declare const bind: <T>(service: string | ServiceFactory | T, container?: ContainerInterface) => Registrar<T>;
 export declare const singleton: <T>(service: string | ServiceFactory | T, container?: ContainerInterface) => Registrar<T>;
 export declare const make: <T>(service: string | ServiceFactory | T, container?: ContainerInterface | undefined) => any;
